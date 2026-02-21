867a4a77d69d62e866a2a5d9fb7c992a7c4879e7
Arnav kumar <arnavaru62@gmail.com>
2026-02-22 01:31:10 +0530

    feat(pipeline): Import voice clone and lipsync layers

--- dub_video.py
+++ dub_video.py
@@ -3,3 +3,6 @@ import os
 from core.media_utils import extract_video_segment, extract_audio
 from core.transcribe import transcribe_audio
 from core.translate import translate_to_hindi
+from core.voice_clone import generate_hindi_dub
+from core.lipsync import run_video_retalking
+
