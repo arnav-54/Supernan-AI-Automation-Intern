253cc79eba11b2795a6cd92065782b58985ec295
Arnav kumar <arnavaru62@gmail.com>
2026-02-22 01:31:10 +0530

    feat(utils): Implement audio separation with ffmpeg

--- core/media_utils.py
+++ core/media_utils.py
@@ -15,3 +15,18 @@ def extract_video_segment(input_path: str, output_path: str, start_time: str, en
     except ffmpeg.Error as e:
         print("FFmpeg encoding error:", e.stderr)
         raise
+
+def extract_audio(video_path: str, audio_path: str):
+    print("Extracting reference audio...")
+    try:
+        (
+             ffmpeg
+             .input(video_path)
+             .output(audio_path, acodec='pcm_s16le', ac=1, ar='22050')
+             .overwrite_output()
+             .run(quiet=True)
+        )
+        return audio_path
+    except ffmpeg.Error as e:
+        print("FFmpeg audio extraction error:", e.stderr)
+        raise
