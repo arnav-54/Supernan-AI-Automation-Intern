522bb435fc2573a928fd2000007c22726cad1123
Arnav kumar <arnavaru62@gmail.com>
2026-02-22 01:31:10 +0530

    feat(pipeline): Import transcribing and translation layers

--- dub_video.py
+++ dub_video.py
@@ -1,3 +1,5 @@
 import argparse
 import os
 from core.media_utils import extract_video_segment, extract_audio
+from core.transcribe import transcribe_audio
+from core.translate import translate_to_hindi
