7e2858516498cd0a36c245f3d638a1e72dc3ea36
Arnav kumar <arnavaru62@gmail.com>
2026-02-22 01:31:10 +0530

    feat(utils): Implement video extraction

--- core/media_utils.py
+++ core/media_utils.py
@@ -1,3 +1,17 @@
 import ffmpeg
 import os
 
+def extract_video_segment(input_path: str, output_path: str, start_time: str, end_time: str):
+    print(f"Extracting video chunk from {start_time} to {end_time}...")
+    try:
+        (
+            ffmpeg
+            .input(input_path, ss=start_time, to=end_time)
+            .output(output_path, c='copy')
+            .overwrite_output()
+            .run(quiet=True)
+        )
+        return output_path
+    except ffmpeg.Error as e:
+        print("FFmpeg encoding error:", e.stderr)
+        raise
