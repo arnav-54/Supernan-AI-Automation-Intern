8c612af86a672a42e6f75974e900fa48b1cf9e87
Arnav kumar <arnavaru62@gmail.com>
2026-02-22 01:31:10 +0530

    feat(translate): Initialize translator logic

--- core/translate.py
+++ core/translate.py
@@ -1,2 +1,6 @@
 from deep_translator import GoogleTranslator
 
+def translate_to_hindi(transcription_segments):
+    print("Translating transcriptions to Hindi...")
+    translator = GoogleTranslator(source='en', target='hi')
+    hindi_segments = []
