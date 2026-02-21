ffafae95f8784d67735b6ec7b9f1d9e20c7f215c
Arnav kumar <arnavaru62@gmail.com>
2026-02-22 01:31:10 +0530

    feat(translate): Integrate DeepTranslator for iterations

--- core/translate.py
+++ core/translate.py
@@ -4,3 +4,14 @@ def translate_to_hindi(transcription_segments):
     print("Translating transcriptions to Hindi...")
     translator = GoogleTranslator(source='en', target='hi')
     hindi_segments = []
+
+    for seg in transcription_segments:
+        # Context-aware translation can be added by swapping to IndicTrans2
+        translated = translator.translate(seg['text'])
+        hindi_segments.append({
+            "start": seg['start'],
+            "end": seg['end'],
+            "en_text": seg['text'],
+            "hi_text": translated
+        })
+    return hindi_segments
